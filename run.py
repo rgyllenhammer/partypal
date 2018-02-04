4b0a67a2a6672660778e01bb832f44252b47b8d7
Reese Gyllenhammer <rgyllenhammer@gmail.com>
2018-02-03 20:00:56 -0800

    made some commands work

--- run.py
+++ run.py
@@ -1,77 +1,80 @@
-# /usr/bin/env python
-# Download the twilio-python library from twilio.com/docs/libraries/python
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+
+# globals
 app = Flask(__name__)
-app.config.from_object(__name__)
-import schedule 
-import time
-# Try adding your own number to this list!
-callers = {
-    "+15303215413": "Rey",
+account_sid = 'AC4e13fc094f1123f11509a0fe7c65da7c'
+account_token = '8860bd7669fda81f028652eda833a716'
+client = Client(account_sid, account_token)
+
+session = False
+
+def send_message(to, by, msg):
+    ''' handles sending messges and ensurest that all parameters are strings'''
+
+    to = str(to)
+    by = str(by)
+    msg = str(msg)
+
+    client.api.account.messages.create(
+        to=to,
+        from_=by,
+        body=msg)
+
+
+# command functions
+def start_session():
+    return 'a session has been started for you'
+
+def end_session():
+    return 'session has ended'
+
+def send_help():
+    return 'sending help'
+
+def hehe():
+    return 'oh shit waddup its dat boi'
+
+def show_commands():
+    return '''
+        start: starts a session for a party
+        end: ends a session for a party
+        help: immediately calls your number one contact for help
+        man: displays command
+     '''
+
+commands = {
+    "start": start_session(),
+    "end": end_session(),
+    "help": send_help(),
+    "easteregg": hehe(),
+    "man": show_commands()
+
 }
 
-
-@app.route("/", methods=['GET', 'POST'])
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
     # Start our response
-    from twilio.twiml.messaging_response import MessagingResponse
-    from datetime import date
-    from twilio.rest import Client
-
-    # Your Account Sid and Auth Token from twilio.com/console
-    account_sid = "AC4e13fc094f1123f11509a0fe7c65da7c"
-    auth_token = "8860bd7669fda81f028652eda833a716"
-    client = Client(account_sid, auth_token)
-
-    body = request.values.get('Body', None)  
+    body = request.values.get('Body', None)
     resp = MessagingResponse()
-    
-    if body == "ok":
-        resp.message("Your friends have been notified")
-        friend_notification()
+
+    # Add a message
+    if body[0] == '[':
+
+        endInd = body.index(']')
+        cmd = body[1:endInd]
+
+        resp.message(commands[cmd])
     else:
-        resp.message("Keep raving")
-            
+        if session:
+            resp.message('''no command detetected. start a command with '[' and end it with ']'. use the command 'man' to see a list of commands and their functions''')
+    print(session)
+
     return str(resp)
 
 
-def send_first_message():
-    from twilio.rest import Client
-
-    # Find these values at https://twilio.com/user/account
-    account_sid = "AC4e13fc094f1123f11509a0fe7c65da7c"
-    auth_token = "8860bd7669fda81f028652eda833a716"
-
-    client = Client(account_sid, auth_token)
-
-    client.api.account.messages.create(
-        to="+15303215413",
-        from_="+18315081228",
-        body="How's the party going?")
-
-
-
-def friend_notification():
-    from twilio.rest import Client
-
-    # Find these values at https://twilio.com/user/account                      
-    account_sid = "AC4e13fc094f1123f11509a0fe7c65da7c"
-    auth_token = "8860bd7669fda81f028652eda833a716"
-
-    client = Client(account_sid, auth_token)
-
-    client.api.account.messages.create(
-        to="+12068547564",
-        from_="+18315081228",
-        body="Your friend Lindsey is at a party and needs help.")
-
-send_first_message()
-
-
 if __name__ == "__main__":
+
     app.run(debug=True)
-       
