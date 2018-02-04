bac94e6592551c7ea3d6d8bb6c45469c27f9cbd5
Reese Gyllenhammer <rgyllenhammer@gmail.com>
2018-02-04 08:18:44 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 from twilio.rest import Client
+from threading import Thread
+import time
 
 # globals
 app = Flask(__name__)
@@ -44,6 +46,12 @@ def show_commands():
         man: displays command
      '''
 
+def check_in(session):
+    while session:
+        send_message('+18314211860', '+18315081228', 'How is the party going?')
+        time.sleep(30)
+        i += 1
+
 commands = {
     "start": start_session(),
     "end": end_session(),
@@ -53,10 +61,11 @@ commands = {
 
 }
 
+
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
-    """Respond to incoming messages with a friendly SMS."""
-    # Start our response
+    global session
+
     body = request.values.get('Body', None)
     resp = MessagingResponse()
 
@@ -67,6 +76,13 @@ def sms_ahoy_reply():
         cmd = body[1:endInd]
 
         resp.message(commands[cmd])
+
+    elif body == 'start':
+        session = True
+
+    if session:
+        Thread(target = check_in, args=[session]).start()
+
     else:
         if session:
             resp.message('''no command detetected. start a command with '[' and end it with ']'. use the command 'man' to see a list of commands and their functions''')
@@ -76,5 +92,4 @@ def sms_ahoy_reply():
 
 
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    Thread(target = app.run(debug=True)).start()
