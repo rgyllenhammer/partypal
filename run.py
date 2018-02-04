1acb1d2d112aff060167d11ecc4b0689c0983ca6
Reese Gyllenhammer <rgyllenhammer@gmail.com>
2018-02-04 09:26:02 -0800

    Update run.py

--- run.py
+++ run.py
@@ -12,6 +12,13 @@ client = Client(account_sid, account_token)
 
 session = False
 
+friendslist = [
+    ['halina', '+12068547564'],
+    ['kevin', '+14088722549'],
+    ['lindsey', '+15303215413'],
+    ['reese', '+18314211860'],
+]
+
 def send_message(to, by, msg):
     ''' handles sending messges and ensurest that all parameters are strings'''
 
@@ -27,16 +34,22 @@ def send_message(to, by, msg):
 
 # command functions
 def start_session():
-    return 'a session has been started for you'
+    return 'a session has been started for you', True #starts session
 
 def end_session():
-    return 'session has ended'
+    return 'session has ended', False
 
-def send_help():
-    return 'sending help'
+def send_help(friends = friendslist):
+    for friend in friends:
+        name = friend[0]
+        number = friend[1]
+
+        send_message(number, '+18315081228', 'hello {}, your friend {} is in need of help at a party'.format(name, 'reese'))
+
+    return 'friends are on the way', True
 
 def hehe():
-    return 'oh shit waddup its dat boi'
+    return 'oh shit waddup its dat boi', True
 
 def show_commands():
     return '''
@@ -44,27 +57,26 @@ def show_commands():
         end: ends a session for a party
         help: immediately calls your number one contact for help
         man: displays command
-     '''
+     ''', True
 
 def check_in(session):
     while session:
         send_message('+18314211860', '+18315081228', 'How is the party going?')
         time.sleep(30)
-        i += 1
+
 
 commands = {
     "start": start_session(),
     "end": end_session(),
-    "help": send_help(),
     "easteregg": hehe(),
     "man": show_commands()
 
 }
 
-
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     global session
+    global friendslist
 
     body = request.values.get('Body', None)
     resp = MessagingResponse()
@@ -75,10 +87,11 @@ def sms_ahoy_reply():
         endInd = body.index(']')
         cmd = body[1:endInd]
 
-        resp.message(commands[cmd])
-
-    elif body == 'start':
-        session = True
+        if cmd == 'help':
+            send_help(friendslist)
+        else:
+            reply, session = commands[cmd]
+            resp.message(reply)
 
     if session:
         Thread(target = check_in, args=[session]).start()
